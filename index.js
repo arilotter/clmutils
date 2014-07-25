f17f5d5dbc9c0621353d3a183c65ba6aea29d5f7
Tomasz Dysinski <tom@jam3.com>
2014-07-25 18:01:03 -0400

    made halo optional

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
 	faceOrientation: require( './lib/faceOrientation' ),
 	getFaceSize: require( './lib/getFaceSize' ),
 	textureCapture: require( './lib/textureCapture' ),
-	addForehead: require( './lib/addForehead' )
+	forehead: require( './lib/forehead' ),
+	halo: require( './lib/halo' )
 };
